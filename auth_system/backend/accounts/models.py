fa92af535d77ceb2e8725eb38ab32fc68a8e580e
Blessman-Newton <blessmannewton0@gmail.com>
2023-07-02 04:30:01 +0100

    fix the models

--- auth_system/backend/accounts/models.py
+++ auth_system/backend/accounts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
+
 class UserManagerAccount(BaseUserManager):
     def create_user(self, email, name, password=None):
         if not email:
