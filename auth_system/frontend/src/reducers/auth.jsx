f36ae557a1995d11f36f9212bee87c53b52867fd
Blessman-Newton <blessmannewton0@gmail.com>
2023-08-19 01:42:38 +0100

    Fix function name error

--- auth_system/frontend/src/reducers/auth.jsx
+++ auth_system/frontend/src/reducers/auth.jsx
@@ -1,3 +1,53 @@
+// import {
+//     LOGIN_SUCCESS,
+//     LOGIN_FAIL,
+//     LOGIN_LOADED_SUCCESS,
+//     LOGIN_LOADED_FAIL,
+// } from '../actions/types'
+
+// const initialStates = {
+//     access: localStorage.getItem('access'),
+//     refresh: localStorage.getItem('refresh'),
+//     isAuthenticated: null,
+//     user: null
+// };
+
+// export default function(state = initialStates, action) {
+//     const { type, payload } = action
+
+//     switch(type) {
+//         case LOGIN_SUCCESS:
+//             localStorage.setItem('access', payload.access);
+//             return {
+//                 ...state,
+//                 isAuthenticated: true, 
+//                 access: payload.access,
+//                 refresh: payload.refresh,
+//         }
+//         case LOGIN_LOADED_SUCCESS:
+//             return {
+//                 ...state,
+//                 user: payload,
+//         }
+//         case LOGIN_LOADED_FAIL:
+//             return {
+//                 ...state,
+//                 user: null,
+//         }
+//         case LOGIN_FAIL:
+//             localStorage.removeItem('access');
+//             localStorage.removeItem('refresh');
+//             return {
+//                 ...state,
+//                 isAuthenticated: false, 
+//                 access: null,
+//                 refresh: null,
+//                 user: null,
+//         }
+//     }
+// }
+
+
 import {
     LOGIN_SUCCESS,
     LOGIN_FAIL,
@@ -12,10 +62,10 @@ const initialStates = {
     user: null
 };
 
-export default function(state = initialStates, action) {
+export default function myauths(state = initialStates, action) {
     const { type, payload } = action
 
-    switch (type) {
+    switch(type) {
         case LOGIN_SUCCESS:
             localStorage.setItem('access', payload.access);
             return {
@@ -23,7 +73,17 @@ export default function(state = initialStates, action) {
                 isAuthenticated: true, 
                 access: payload.access,
                 refresh: payload.refresh,
-        }
+            };
+        case LOGIN_LOADED_SUCCESS:
+            return {
+                ...state,
+                user: payload,
+            };
+        case LOGIN_LOADED_FAIL:
+            return {
+                ...state,
+                user: null,
+            };
         case LOGIN_FAIL:
             localStorage.removeItem('access');
             localStorage.removeItem('refresh');
@@ -32,16 +92,9 @@ export default function(state = initialStates, action) {
                 isAuthenticated: false, 
                 access: null,
                 refresh: null,
-        }
-         case LOGIN_LOADED_SUCCESS:
-            return {
-                ...state,
-                user: payload,
-        }
-        case LOGIN_LOADED_FAIL:
-            return {
-                ...state,
                 user: null,
-        }
+            };
+        default:
+            return state; // Handling the default case by returning the current state
     }
 }
